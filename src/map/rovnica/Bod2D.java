5d26a1c17f0cbf1d84437619e7daeac001d75e9c
DLIMIKO <matus.necas@gmail.com>
2020-03-05 10:36:05 +0100

    vytvorenie triedy Bod2d z rozhrania

--- src/map/rovnica/Bod2D.java
+++ src/map/rovnica/Bod2D.java
@@ -5,56 +5,73 @@ public class Bod2D implements Bod
     float x;
     float y;
 
-
-
-
-    @Override
-    public float getX() {
-        return 0;
-    }
-
-    @Override
-    public float getY() {
-        return 0;
-    }
-
-    @Override
-    public float[] getXY() {
-        return new float[0];
-    }
-
-    @Override
-    public void setX(float X) {
+    public Bod2D(float X, float Y)
+    {
+        this.x=X;
+        this.y=Y;
 
     }
 
-    @Override
-    public void setY(float Y) {
 
+    @Override
+    public float getX()
+    {
+        return x;
     }
 
     @Override
-    public void setXY(float... suradnice) {
-
+    public float getY()
+    {
+        return y;
     }
 
     @Override
-    public float getDistance(Bod other) {
-        return 0;
+    public float[] getXY()
+    {
+        return new float[]{x,y};
     }
 
     @Override
-    public Bod Plus(Bod other) {
-        return null;
+    public void setX(float X)
+    {
+        this.x=X;
     }
 
     @Override
-    public Bod Minus(Bod other) {
-        return null;
+    public void setY(float Y)
+    {
+        this.y=Y;
     }
 
     @Override
-    public boolean equals(Bod other) {
-        return false;
+    public void setXY(float... suradnice)
+    {
+        this.x=suradnice[0];
+        this.y=suradnice[1];
+    }
+
+    @Override
+    public float getDistance(Bod other)
+    {
+        return (float )Math.sqrt(Math.pow(this.x-other.getX(),2)+Math.pow(this.y-other.getY(),2));
+    }
+
+    @Override
+    public Bod Plus(Bod other)
+    {
+
+        return new Bod2D(this.x+other.getX(), this.y+other.getY());
+    }
+
+    @Override
+    public Bod Minus(Bod other)
+    {
+        return new Bod2D(this.x-other.getX(), this.y-other.getY());
+    }
+
+    @Override
+    public boolean equals(Bod other)
+    {
+        return(this.x==other.getX() && this.y==other.getY());
     }
 }
---
+++ src/test_Bod2D/Bod2DTest.java
@@ -0,0 +1,31 @@
+package test_Bod2D;
+
+
+import map.rovnica.Bod2D;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Bod2DTest {
+
+   Bod2D Prvy=new Bod2D(1,2);
+   Bod2D Druhy=new Bod2D(0,0);
+
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minus() {
+    }
+}
